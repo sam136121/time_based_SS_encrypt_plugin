491e8742c3ea0fd31f2dbd2870bda1d9c8cb8991
sam136121 <sam136121@126.com>
2019-04-26 22:06:25 +0800

    update readme file and fix a commond lost.

--- update_ss_client.sh
+++ update_ss_client.sh
@@ -2,5 +2,6 @@
 PID=`ps -w | grep "ssserver -c /etc/shadowsocks.json -d start"|grep -v grep $1| awk '{print $1}'`
 kill -9 $PID
 sleep 2
+python /root/encrypt_by_time_server.py
 ssserver -c /etc/shadowsocks.json -d start
 
